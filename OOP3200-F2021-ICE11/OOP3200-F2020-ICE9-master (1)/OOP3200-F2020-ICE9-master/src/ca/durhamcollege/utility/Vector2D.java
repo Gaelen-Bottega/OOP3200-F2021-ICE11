258a7ec490672440e6548aa54d950a7d49e25bfa
Gaelen Rhoads <gaelenrhoads@gmail.com>
2021-12-03 23:25:11 -0500

    Removed Random vector and added random functionality to Vector2D

--- OOP3200-F2021-ICE11/OOP3200-F2020-ICE9-master (1)/OOP3200-F2020-ICE9-master/src/ca/durhamcollege/utility/Vector2D.java
+++ OOP3200-F2021-ICE11/OOP3200-F2020-ICE9-master (1)/OOP3200-F2020-ICE9-master/src/ca/durhamcollege/utility/Vector2D.java
@@ -3,8 +3,8 @@ package ca.durhamcollege.utility;
 public class Vector2D
 {
     // PRIVATE INSTANCE VARIABLES
-    private float x;
-    private float y;
+    protected float x;
+    protected float y;
 
     // PUBLIC PROPERTIES (MUTATORS & ACCESSORS)
     public float getX()
@@ -90,7 +90,7 @@ public class Vector2D
 
     public float getMagnitude()
     {
-        return (float)(Math.sqrt(this.getX() * this.getX() + this.getY() * this.getY()));
+        return (float)(Mathf.Sqrt(this.getX() * this.getX() + this.getY() * this.getY()));
     }
 
     public float getSqrMagnitude()
@@ -110,9 +110,8 @@ public class Vector2D
 
     public void normalize()
     {
-        final float magnitude;
-        magnitude = this.getMagnitude();
-        if ((double)(magnitude) > 9.99999974737875E-06)
+        final float magnitude = this.getMagnitude();
+        if (magnitude > 9.99999974737875E-06)
         {
             set(getX() / magnitude, getY() / magnitude);
         }
@@ -189,11 +188,24 @@ public class Vector2D
 
     public static float distance(final Vector2D a, final Vector2D b)
     {
-        final double delta_x;
-        delta_x = (double)(b.getX()) - (double)(a.getX());
-        final double delta_y;
-        delta_y = (double)(b.getY()) - (double)(a.getY());
+        final double delta_x = (double)(b.getX()) - (double)(a.getX());
+        final double delta_y = (double)(b.getY()) - (double)(a.getY());
 
-        return (float)(Math.sqrt(delta_x * delta_x + delta_y * delta_y));
+        return (float)(Mathf.Sqrt(delta_x * delta_x + delta_y * delta_y));
+    }
+
+    public static final Vector2D random(final Vector2D start, final Vector2D end)
+    {
+        // generate random Y value
+        float minX = Mathf.Min(start.getX(), end.getX());
+        float maxX = Mathf.Max(start.getX(), end.getX());
+        float randomX = Mathf.RandomRange(minX, maxX);
+
+        // generate random X value
+        float minY = Mathf.Min(start.getY(), end.getY());
+        float maxY = Mathf.Max(start.getY(), end.getY());
+        float randomY = Mathf.RandomRange(minY, maxY);
+
+        return new Vector2D(randomX, randomY);
     }
 }
